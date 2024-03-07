3e836278d49613bc38550867ff0b12576ad9e281
Ankur586 <ankur.singh586@gmail.com>
2024-03-07 21:13:19 +0530

    form

--- ajax_form/views.py
+++ ajax_form/views.py
@@ -12,6 +12,10 @@ def home(request):
 def contact_us_page(request):
     return render(request,'contact_us.html')
     
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+# from .forms import ContactForm
+
 def contact_page_form(request):
     if request.method == 'POST':
         # Process form data
@@ -20,15 +24,17 @@ def contact_page_form(request):
         email_address = request.POST.get('email_address')            
         website_name = request.POST.get('website_name')
         message1 = request.POST.get('customer_message')
-        try :
+        try:
             validate_email(email_address)
-            contact_us(first_name = first_name1, last_name=last_name1, email_address = email_address, website_name = website_name, customer_message = message1).save()
+            contact_us(first_name=first_name1, last_name=last_name1, email_address=email_address, website_name=website_name, customer_message=message1).save()
             return JsonResponse({'message': 'Form submitted successfully!'})
         except ValidationError as e:
-            messages.error(request, "Please Enter a Valid Email Address")
-            return JsonResponse({'error': 'Invalid request method.'}, status=400)
+            error_messages = [str(error) for error in e.messages]
+            # messages.error(request, error_messages[0])  # Display first error message
+            return JsonResponse({'error': error_messages}, status=400)
     else:
         return JsonResponse({'error': 'Invalid request method.'}, status=400)
+
     
     
 # def validate_email_address(request):
